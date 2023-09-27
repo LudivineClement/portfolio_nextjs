fec1834c8c39a72514bb010555b888550bc200bd
ludivineclement <ludivine.clement0985@gmail.com>
2023-09-27 18:05:02 +0200

    changement présentation projet

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faUpRightFromSquare, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const ProjectModal = ({ data, isOpen, setIsOpen }) => {
 
@@ -48,7 +48,8 @@ const ProjectModal = ({ data, isOpen, setIsOpen }) => {
               <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-pink-light group-hover:h-full ease"></span>
               <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-pink-light group-hover:h-full ease"></span>
               <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-pink-global opacity-0 group-hover:opacity-100"></span>
-              <span className="relative inline-flex items-center gap-2 transition-colors duration-300 delay-200 group-hover:text-white ease">Lien Github <FontAwesomeIcon icon={faArrowRightFromBracket} className='text-sm transition-colors duration-300 delay-200 ease text-gray-global group-hover:text-white' /></span></a>
+              <span className="relative inline-flex items-center gap-2 transition-colors duration-300 delay-200 group-hover:text-white ease">Lien Github <FontAwesomeIcon icon={faUpRightFromSquare} className='text-xs transition-colors duration-300 delay-200 ease text-gray-global group-hover:text-white' /></span></a>
+              
             {data.website ? (
               <a href={data.website} target="_blank" className="relative px-4 py-[10px] ml-4 overflow-hidden font-medium text-gray-global text-sm group-hover:text-white bg-gray-100 border border-gray-100 rounded-xl shadow-inner group">
                 <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-pink-light group-hover:w-full ease"></span>
@@ -56,7 +57,7 @@ const ProjectModal = ({ data, isOpen, setIsOpen }) => {
                 <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-pink-light group-hover:h-full ease"></span>
                 <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-pink-light group-hover:h-full ease"></span>
                 <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-pink-global opacity-0 group-hover:opacity-100"></span>
-                <span className="relative inline-flex items-center gap-2 transition-colors duration-300 delay-200 group-hover:text-white ease">Lien vers le site  <FontAwesomeIcon icon={faArrowRightFromBracket} className='text-sm transition-colors duration-300 delay-200 ease text-gray-global group-hover:text-white' /></span></a>
+                <span className="relative inline-flex items-center gap-2 transition-colors duration-300 delay-200 group-hover:text-white ease">Lien vers le site  <FontAwesomeIcon icon={faUpRightFromSquare} className='text-xs transition-colors duration-300 delay-200 ease text-gray-global group-hover:text-white' /></span></a>
             ) : null}
           </div>
 
