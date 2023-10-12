79e58dc9486e0df93b8ac79871a3204590e2d6cf
ludivineclement <ludivine.clement0985@gmail.com>
2023-10-12 17:39:26 +0200

    remove grayscale mobile

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -12,9 +12,9 @@ const ProjectCard = ({ img, title, github, url, ToggleProjectInfo }) => {
         src={img}
         alt={title}
         onClick={ToggleProjectInfo}
-        className='w-full h-full object-cover grayscale transition-transform duration-[400ms] ease-out group-hover:scale-105 group-hover:grayscale-0'
+        className='w-full h-full object-cover lg:grayscale transition-transform duration-[400ms] ease-out group-hover:lg:scale-105 group-hover:lg:grayscale-0'
       />
-      <div className='absolute w-full bg-pink-global h-0 z-10  cursor-default -bottom-5 p-4 group-hover:lg:h-[100px] group-hover:lg:opacity-80 group-hover:lg:bottom-0 transition-bottom duration-500 ease-in-out opacity-90'>
+      <div className='absolute w-full bg-pink-global h-0 z-10 lg:cursor-default -bottom-5 p-4 group-hover:lg:h-[100px] group-hover:lg:opacity-80 group-hover:lg:bottom-0 transition-bottom duration-500 ease-in-out opacity-90'>
         <h3 className='text-[1.7rem] text-center opacity-100 text-black font-kaushan font-semibold'>{title}</h3>
         <div className='flex justify-center m-3 items-center'>
           <div title='Voir plus' onClick={ToggleProjectInfo}><FontAwesomeIcon icon={faCirclePlus} className='text-black text-lg lg:text-base mx-2 lg:mx-[5px] transition-transform duration-200 opacity-100 ease-in hover:scale-125 cursor-pointer' /></div>
