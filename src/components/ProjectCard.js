5a1d89d65426e593a0969a5dfb035c60795ab60f
ludivineclement <ludivine.clement0985@gmail.com>
2023-10-12 17:34:02 +0200

    amélioration clic card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,13 +7,14 @@ import { BsGithub } from 'react-icons/bs';
 const ProjectCard = ({ img, title, github, url, ToggleProjectInfo }) => {
  
   return (
-    <div className="bg-white group relative overflow-hidden h-[400px] shadow-sm will-change-transform">
+    <div className="bg-white group relative overflow-hidden h-[400px] cursor-pointer shadow-sm will-change-transform">
       <img
         src={img}
         alt={title}
+        onClick={ToggleProjectInfo}
         className='w-full h-full object-cover grayscale transition-transform duration-[400ms] ease-out group-hover:scale-105 group-hover:grayscale-0'
       />
-      <div className='absolute w-full bg-pink-global h-0 z-10 -bottom-5 p-4 group-hover:h-[100px] group-hover:opacity-80 group-hover:bottom-0 transition-bottom duration-500 ease-in-out opacity-90'>
+      <div className='absolute w-full bg-pink-global h-0 z-10  cursor-default -bottom-5 p-4 group-hover:lg:h-[100px] group-hover:lg:opacity-80 group-hover:lg:bottom-0 transition-bottom duration-500 ease-in-out opacity-90'>
         <h3 className='text-[1.7rem] text-center opacity-100 text-black font-kaushan font-semibold'>{title}</h3>
         <div className='flex justify-center m-3 items-center'>
           <div title='Voir plus' onClick={ToggleProjectInfo}><FontAwesomeIcon icon={faCirclePlus} className='text-black text-lg lg:text-base mx-2 lg:mx-[5px] transition-transform duration-200 opacity-100 ease-in hover:scale-125 cursor-pointer' /></div>
